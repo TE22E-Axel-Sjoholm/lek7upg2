b66e1a6dd56c876cdebd8f9b4b433b40ac627e42
Axel Sjöholm <Axel.sjoholm@elev.ga.ntig.se>
2023-10-11 10:25:34 +0200

    del b, jämför tal1 o 2 och säger om 2 e större

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+       int tal1 = 10;
+       int tal2 = 20;
+       if(tal2>tal1){
+        System.out.println(tal2+" är större än "+tal1);
+       }
+    }
+}
